ae1f3ae9626a5993aacad4ed46d389c50d0bab84
apoorvaa <apoorvaagowda08@gmail.com>
2023-06-21 06:49:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import CoverPage from './images/CoverPage.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="leftSection">
+         <img src={CoverPage} alt="main-picture"/>
+         <h1  className="leftSection"> Discover new things on Superapp</h1>
+       </div>
+       <div className="rightSection">
+        <form  className="submission">
+        <input type="name" placeholder="Name" className="submission" />
+        <input type="username" placeholder="UserName"  className="submission"/>
+        <input type="email" placeholder="Email"  className="submission" />
+        <input type="mobile" placeholder="Mobile"  className="submission"/>
+        
+        </form>
+       </div>
     </div>
-  );
-}
-
+  )
+    
+  }
 export default App;
